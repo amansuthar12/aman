7fc49b213f4a554295ba9edb5f76fdc80e7905bc
amansuthar12 <amanallbackup@gmail.com>
2024-01-06 20:25:51 +0530

    zman

---
+++ giittt.cpp
@@ -0,0 +1,14 @@
+<<<<<<< HEAD
+=======
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "aman loves janhvi ";
+
+:diffg RE    return 0;
+    //  this is code
+    
+
+}
+>>>>>>> aman1
